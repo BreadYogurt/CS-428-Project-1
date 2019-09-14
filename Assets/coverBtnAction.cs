04e5e1cbc9a42cb91e054029ea0ab98374bb6b97
Brent Yurek <brentagainagain@gmail.com>
2019-09-14 18:32:13 -0500

    Finished Alpha

--- Assets/coverBtnAction.cs
+++ Assets/coverBtnAction.cs
@@ -5,19 +5,25 @@ using System.Collections.Generic;
 
 public class coverBtnAction : MonoBehaviour, IVirtualButtonEventHandler {
     public GameObject vbBtnObj;
+    public GameObject actor;
 
     // Start is called before the first frame update
     void Start() {
         vbBtnObj = GameObject.Find("coverBtn");
         vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        actor = GameObject.Find("Jimmy Jett");
+        actor.SetActive(false);
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb) {
         Debug.Log("Button pressed!");
+        actor.SetActive(true);
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb) {
         Debug.Log("Button released!");
+        actor.SetActive(false);
     }
 
     // Update is called once per frame
