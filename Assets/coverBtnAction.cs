016462d6b9cb334b923766fb32e7eb7eec1b8862
Brent Yurek <brentagainagain@gmail.com>
2019-09-14 11:59:32 -0500

    Initial Commit

---
+++ Assets/coverBtnAction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+using System.Collections.Generic;
+
+public class coverBtnAction : MonoBehaviour, IVirtualButtonEventHandler {
+    public GameObject vbBtnObj;
+
+    // Start is called before the first frame update
+    void Start() {
+        vbBtnObj = GameObject.Find("coverBtn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb) {
+        Debug.Log("Button pressed!");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        Debug.Log("Button released!");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
